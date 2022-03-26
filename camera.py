e1e21f86dab865582272aeb9b259c3dce1ee4bb9
nyssha <65846349+nyssha@users.noreply.github.com>
2022-03-26 18:37:42 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,48 @@
+from tkinter import image_names
+import cv2
+import dropbox
+import time
+import random
+start_time=time.time()
+
+
+def take_snapshot():
+    number=random.randint(0,100)
+    videoCaptureObject=cv2.VideoCapture(0)
+    result=True
+    while(result):
+        ret,frame=videoCaptureObject.read()
+        img_name="img"+str(number)+".png"
+        cv2.imwrite(img_name,frame)
+        start_time=time.time
+        result=False
+
+    return img_name
+    print("snapshot taken")
+
+
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()     
+
+def upload_files(img_name):
+    access_token="sl.BDOSqFweTR3nkamLKgEEbxd7KkFLTbuJ8dYOVNxTNGErAJjDAGBaU5Dwg59NGX2X171akPfCM-xPG5e95j6k4zINuxW22TihHmIiF2OgEVyLABbwH91k3iNNpEmqJ71G2ZGRERQ"
+    file=img_name
+    file_from=file
+    file_to="/testFolder1/"+(img_name)
+    dbx=dropbox.Dropbox(access_token)
+
+    with open(file_from,'rb') as f:
+        dbx.file_upload(f.read(),file_to,mode=dropbox.files.WriteMode.overwrite)  
+        print("file uploaded")
+
+
+def main():
+    while(True):
+        if((time.time() -start_time) >=300):
+            name=take_snapshot()
+            upload_files(name)
+                    
+
+
+
+main()
